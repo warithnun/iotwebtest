ebdb5391dd0f79acef2252357623559cd2310e89
thanit <144163583+Thanit1@users.noreply.github.com>
2024-03-24 00:27:15 +0700

    Update database.js

--- database.js
+++ database.js
@@ -6,5 +6,13 @@ const dbConnection = mysql.createConnection({
     password: 'HDKkbe38334', // MySQL password
     database: 'web01' // MySQL database name
 });
+dbConnection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL: ' + err.stack);
+        return;
+    }
+    console.log('Connected to MySQL as id ' + dbConnection.threadId);
+});
+
 
 module.exports = dbConnection;
