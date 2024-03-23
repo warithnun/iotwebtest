de2a2d8fed107f796305d3ac59162248ac2105b2
thanit <144163583+Thanit1@users.noreply.github.com>
2024-03-24 02:56:46 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ var con = mysql.createConnection({
 });
 
 con.connect(function(err) {
-  if (err) throw err;
-  console.log("You are connected!");
-});
+    if (err) throw err;
+    console.log("You are connected!");
+  
+    // เพิ่มโค้ดดึงข้อมูลออกมาจากฐานข้อมูลที่นี่
+    con.query("SELECT * FROM your_table", function (err, result, fields) {
+      if (err) throw err;
+      console.log(result); // แสดงผลลัพธ์ที่ได้จากการ query
+    });
+  });
+  
 con.end();
