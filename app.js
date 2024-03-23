b5afe78bb5f8141d9058170fd1224e4857055864
thanit <144163583+Thanit1@users.noreply.github.com>
2024-03-24 03:13:58 +0700

    t

--- app.js
+++ app.js
@@ -1,22 +1,38 @@
+var express = require('express');
 var mysql = require('mysql');
+var ejs = require('ejs');
+var bodyParser = require('body-parser');
+
+var app = express();
+
 var con = mysql.createConnection({
-  host: "node60691-env-7996996.th1.proen.cloud",
-  user: "root",
-  password: "VDAygb99771",
-  database: "test"
+    host: "node60691-env-7996996.th1.proen.cloud",
+    user: "root",
+    password: "VDAygb99771",
+    database: "test"
 });
 
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("You are connected!");
-  
-    // เพิ่มโค้ดดึงข้อมูลออกมาจากฐานข้อมูลที่นี่
-    con.query("SELECT * FROM users", function (err, result, fields) {
-      if (err) throw err;
-      console.log(result); // แสดงผลลัพธ์ที่ได้จากการ query
-      
-      // สิ้นสุดการเชื่อมต่อกับฐานข้อมูล MySQL หลังจากทำงานเสร็จสิ้น
-      con.end();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function(req, res) {
+    con.query("SELECT * FROM users", function(err, result, fields) {
+        if (err) throw err;
+        res.render('index', { users: result });
     });
-  });
-  
+});
+
+app.post('/', function(req, res) {
+    var name = req.body.name;
+    var email = req.body.email;
+    
+    con.query("INSERT INTO users (name, email) VALUES (?, ?)", [name, email], function(err, result) {
+        if (err) throw err;
+        console.log("1 record inserted");
+        res.redirect('/'); 
+    });
+});
+
+var server = app.listen(3000, function() {
+    console.log('Server is running on port 3000');
+});
