d513fc1de97f240d42a236b36163169dbfa58b9c
thanit <144163583+Thanit1@users.noreply.github.com>
2024-03-24 03:43:21 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const port = 11512;
 
 app.set('view engine', 'ejs');
 
-
 var con = mysql.createConnection({
     host: "node60691-env-7996996.th1.proen.cloud",
     user: "root",
@@ -16,11 +15,10 @@ var con = mysql.createConnection({
 });
 
 app.get('/', function(req, res) {
-   
-        con.query("SELECT * FROM users", function(err, result) {
-            if (err) throw err;
-            res.render('index', { users: result });
-        });
+     con.query("SELECT * FROM users", function(err, result) {
+        if (err) throw err;
+         res.render('index', { users: result });
+    });
 });
 app.post('/', function(req, res) {
     var name = req.body.name;
@@ -32,7 +30,6 @@ app.post('/', function(req, res) {
         res.redirect('/'); 
     });
 });
-
 
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
