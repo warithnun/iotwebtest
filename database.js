19425814b91420d01a166ddec4a1d2e0be662edb
thanit <144163583+Thanit1@users.noreply.github.com>
2024-03-24 00:22:57 +0700

    t

--- database.js
+++ database.js
@@ -1,11 +1,10 @@
-const { Pool } = require('pg');
+const mysql = require('mysql');
 
-const dbConnection = new Pool({
-    user: 'webadmin', 
-    host: '10.104.10.202',
-    database: 'postgres',
-    password: 'TZOkpx87811',
-    port: 5432
+const dbConnection = mysql.createConnection({
+    host: 'node60688-iotweb.th1.proen.cloud',
+    user: 'root', // MySQL username
+    password: 'HDKkbe38334', // MySQL password
+    database: 'web01' // MySQL database name
 });
 
 module.exports = dbConnection;
