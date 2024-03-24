4682dd757974c147008098d8ccb666e8b971aaaf
thanit <144163583+Thanit1@users.noreply.github.com>
2024-03-24 13:18:47 +0700

    reset

--- app.js
+++ app.js
@@ -1,35 +1,23 @@
 const express = require('express');
+const path = require('path');
 const mysql = require('mysql');
-const ejs = require('ejs')
-const app = express();
-const port = 11512;
+
 
 app.set('view engine', 'ejs');
+
 
 var con = mysql.createConnection({
     host: "node60691-env-7996996.th1.proen.cloud",
     user: "root",
     password: "VDAygb99771",
     database: "test"
-});
+  });
 
-app.get('/', function(req, res) {
-    const sql = "SELECT * FROM users"
+  app.get('/', function(req, res) {
+    const sql = "SELECT * FROM userprofile";
     con.query(sql, function(err, result) {
-        if (err) throw err;
-        res.render('index', { users: result });
+      if (err) throw err;
+      res.render('pages/index', { data: result });
     });
-});
-/* app.post('/add', function(req, res) {
-    var name = req.body.name;
-    var email = req.body.email;
-    
-    con.query("INSERT INTO users (name, email) VALUES (?, ?)", [name, email], function(err, result) {
-        if (err) throw err;
-        console.log("1 record inserted");
-        res.redirect('/'); 
-    });
-}); */
-
-
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+  });
+  app.listen(port, () => console.log(`Server is running on port ${port}`));
