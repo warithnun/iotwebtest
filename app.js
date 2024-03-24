299f53f68e59ce2b9815f113b22a39cedecc6934
thanit <144163583+Thanit1@users.noreply.github.com>
2024-03-24 14:38:56 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieSession = require('cookie-session');
 const bcrypt = require('bcryptjs')
+const mysql = require('mysql');
 const { body, validationResult } = require('express-validator');
 const dbConnection = require('./database');
 const app = express();
