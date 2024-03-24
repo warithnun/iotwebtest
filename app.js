fff3c7f7d74048bf422e7b6ebc94321bcc82e5d1
thanit <144163583+Thanit1@users.noreply.github.com>
2024-03-24 15:43:43 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ const app = express();
 const port = 11991;
 app.use(express.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
-const dbConnection = require('./database');
 
 const pool = new Pool({
-    user: 'webadmin',   // ตั้งค่า PostgreSQL username ที่ถูกต้อง
+    user: 'webadmin',
     host: 'node60694-iotweb.th1.proen.cloud',
     database: 'postgres',
     password: 'KRCqev36719',
@@ -19,7 +18,11 @@ const pool = new Pool({
 app.get('/', function(req, res) {
     const sql = "SELECT * FROM users";
     pool.query(sql, function(err, result) {
-      if (err) throw err;
+      if (err) {
+        console.error('An error occurred.', err);
+        res.status(500).send('An error occurred.');
+        return;
+      }
       console.log(result.rows);
       res.render('index',{ data: result.rows });
     });
@@ -28,7 +31,3 @@ app.get('/', function(req, res) {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-
-
-  app.listen(port, () => console.log(`Server is running on port ${port}`));
